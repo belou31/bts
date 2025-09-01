2729c97c3d73c6f422e7c1260e7ccdcf84d2c372
belou00 <michel.guillou@tbhc.fr>
2025-09-01 19:05:46 +0200

    debug run dev

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ import 'dotenv/config';                 // ← plus simple et sûr
 import { connectDB } from './loaders/mongoose.js';
 import { buildApp } from './loaders/express.js';
 
-await connectDB();                      // utilise process.env.MONGO_URI
-const app = await buildApp();           // construit l'Express app
-
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = Number(process.env.PORT || 8080);
+
+await connectDB();                      // utilise process.env.MONGO_URI
+const app = await buildApp();           // construit l'Express app
 
 app.listen(PORT, HOST, () => {
   console.log(`[server] ${process.env.APP_ENV || 'dev'} listening on http://${HOST}:${PORT}`);
