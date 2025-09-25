4722e597f7a3fbf9c70e5e5942012c8e310dbf46
belou00 <michel.guillou@tbhc.fr>
2025-09-25 08:21:37 +0200

    fix new subscription

--- scripts/provision-seats.js
+++ scripts/provision-seats.js
@@ -113,6 +113,9 @@ async function main() {
   const unavailableList = [
     // Siège manquant
     'S2-F-013',
+    // Correctif rapide
+    'S5-C-064', 'S5-C-065', 'S5-C-066',
+    'N4B-M-063', 'N4B-M-064', 'N4B-M-065', 'N4B-M-066', 'N4B-M-067', 'N4B-M-068', 'N4B-M-069',
     // Manque de visibilité (12 sièges)
     'S5-E-042', 'S5-E-043', 'S5-E-044', 'S5-E-045',
     'S5-F-048', 'S5-F-049', 'S5-F-050',
